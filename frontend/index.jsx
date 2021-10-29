ff84726cfba999552c6a5f65ea2c7320bd611924
Woodstock <victorhe97@gmail.com>
2021-10-29 15:25:24 -0400

    Generated User migration and set up initial validations and user auth methods in User model.

---
+++ frontend/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root')
+
+
+  ReactDOM.render(
+    <Root />,
+    root
+  )
+})
