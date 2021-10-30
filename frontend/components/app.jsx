f01ef38e7b231d57e4b4e095fec1b5e0156e6bc9
Woodstock <victorhe97@gmail.com>
2021-10-30 17:57:59 -0400

    Set up auth and protected routes.

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
 import GreetingContainer from './greeting/greeting_container'
 import SignupFormContainer from './session_form/signup_form_container'
 import LoginFormContainer from './session_form/login_form_container'
-import { Route } from 'react-router-dom'
+import { AuthRoute, ProtectedRoute } from '../util/route_util'
 
 const App = () => (
   <div>
     <header>
-      <h1>Welcome to Destiny+</h1>
-      {/* <GreetingContainer /> */}
+      <Link to="/" className="header-link">
+        <h1>Welcome to Destiny+</h1>
+      </Link>
+      <GreetingContainer />
     </header>
 
-    <Route exact path="/" component={GreetingContainer} />
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <AuthRoute exact path="/login" component={LoginFormContainer} />
+    <AuthRoute exact path="/signup" component={SignupFormContainer} />
+
+    {/* <ProtectedRoute exact path="/browse" /> */}
+
+    {/* {/* <Route exact path="/" component={GreetingContainer} /> */}
+    {/* <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />  */}
   </div>
 )
 
-// decide on whether we want redirect to spash greeting or not
+// decide on whether we want redirect to splash greeting or not
 
 export default App
