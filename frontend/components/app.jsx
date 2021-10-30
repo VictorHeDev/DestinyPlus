fa19fb8f6ffe483486ac736f7a901b4f314d2fd8
Woodstock <victorhe97@gmail.com>
2021-10-29 22:10:43 -0400

    Login, signup, logout now renders on page. Greeting component first draft complete.

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import GreetingContainer from './greeting/greeting_container'
 
 const App = () => (
   <div>
     <h1>Welcome to Destiny+</h1>
+    <GreetingContainer />
   </div>
 )
 
---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Greeting extends React.Component {
+
+  render () {
+    const { currentUser, logout } = this.props
+    const greeting = currentUser ? (
+      <div>
+        <h1>Greetings { currentUser.email }</h1>
+        <button onClick={() => logout()}>Log Out</button>
+      </div>
+    ) : (
+      <div>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    )
+
+    return (
+      greeting
+    )
+  }
+}
