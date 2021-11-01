59dc82efea91126f835d413c81003f6624e42d94
Woodstock <victorhe97@gmail.com>
2021-11-01 11:07:45 -0400

    Added profiles to routes config under api namespace.

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -1,4 +1,6 @@
 class Api::ProfilesController < ApplicationController
+  before_action :require_logged_in, only: [:index, :create, :update, :show, :destroy]
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show]
     resource :session, only: [:create, :destroy]
+    resources :profiles, only: [:index, :show, :create, :update, :destroy]
   end
 
 end
