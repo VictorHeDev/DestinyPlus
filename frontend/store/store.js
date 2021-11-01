6965fe5215d6b78849890be56bcd62aa4940cb0e
Woodstock <victorhe97@gmail.com>
2021-11-01 14:44:26 -0400

    Configured store to use logger only in development.

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/root_reducer';
 
