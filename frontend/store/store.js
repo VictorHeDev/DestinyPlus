17231b418b80e2514dab5b265feb3fc8bc0168fc
Woodstock <victorhe97@gmail.com>
2021-11-01 14:20:15 -0400

    Added middlewares for development/production.

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,11 +4,19 @@ import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 const configureStore = (preloadedState = {}) =>
   createStore(
     rootReducer,
     preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
+    composeWithDevTools(applyMiddleware(...middlewares))
   );
 
 export default configureStore;
