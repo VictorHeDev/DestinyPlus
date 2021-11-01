a05ed04299e0065de161b26a855b903d9dee7817
Woodstock <victorhe97@gmail.com>
2021-11-01 19:54:52 -0400

    Added the ProfileReducer to the entitesReducer file.

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 
 import users from './users_reducer';
+import profile from './profiles_reducer';
 
 const entitiesReducer = combineReducers({
   users,
-  // profile
+  profile,
   // genre
   // video
 });
