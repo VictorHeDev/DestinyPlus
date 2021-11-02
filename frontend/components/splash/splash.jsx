40ac5ab193cef024941615713c51b6bfe934d867
Woodstock <victorhe97@gmail.com>
2021-11-02 17:02:45 -0400

    Added gradient to the background image.

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,8 +3,15 @@ import React from 'react'
 export default function Splash() {
   return (
     <div>
-      <img className="splash-home-img" src={ window.splashHomeURL } />
-      Hey willis
+      <section className="splash-main-content">
+        {/* <img className="splash-home-img" src={ window.splashHomeURL } /> */}
+        <img className="splash-media-bundle" src={ window.mediaBundleURL } />
+        <button>GET THE DESTINY BUNDLE</button>
+        <p>Stream now. Terms Apply</p>
+
+        <p>Sign up for Destiny+ only.</p>
+      </section>
+
     </div>
   )
 }
