637abe16fc56faef08ff9d5033ee862c11f27167
Woodstock <victorhe97@gmail.com>
2021-11-04 16:58:25 -0400

    Completed genre model first draft.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: genres
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Genre < ApplicationRecord
-  
+  validates :name, presence: true
+
+  # has_many :video_genres,
+  #   primary_key: :id,
+  #   foreign_key: :genre_id,
+  #   class_name: :VideoGenres
+
+  # has_many :videos,
+  #   through: :video_genres,
+  #   source: :video
+
 end
