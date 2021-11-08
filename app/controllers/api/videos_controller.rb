12e8db7c6fcfda4982c57773beee6df558a8fdbf
Woodstock <victorhe97@gmail.com>
2021-11-08 16:40:51 -0500

    Commented attached thumbnails back in.

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,9 +1,9 @@
 class Api::VideosController < ApplicationController
   def index
     # @videos = Video.all.includes(:genres)
-    # @videos = Video.with_attached_thumbnail.all.includes(:genres)
+    @videos = Video.with_attached_thumbnail.all.includes(:genres)
 
-    @videos = Video.all
+    # @videos = Video.all
     render :index
   end
 
