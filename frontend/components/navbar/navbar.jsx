40b24718e84ea81125cf6427bd61f14ff7b1c7db
Woodstock <victorhe97@gmail.com>
2021-11-09 22:35:37 -0500

    Added Link tags around destiny plus logo.

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom'
 import NavBarLinks from './navbar_links'
 import AccountContainer from './account_container'
 
-
 export default class NavBar extends React.Component {
 
   render () {
     return (
       <nav className="nav-bar">
         <div className="main-nav">
-          <img className="navLogo" src={ window.destinyLogoURL } />
+          <Link to="/browse">
+            <img
+              // onClick={ () => this.handleLogoClick }
+              className="navLogo"
+              src={ window.destinyLogoURL }
+            />
+          </Link>
           <NavBarLinks />
         </div>
         <AccountContainer currentUser={ this.props.currentUser }/>
--- frontend/components/navbar/navbar_links.jsx
+++ frontend/components/navbar/navbar_links.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouseUser,
+  faSearch,
+  faPlus,
+  faFilm,
+  faTv
+} from '@fortawesome/free-solid-svg-icons'
+
 
 export default function NavBarLinks() {
+  // might have to refactor this later to use NavLinks
   return (
     <div className="navbar-links">
-      <span>HOME</span>
-      <span>SEARCH</span>
-      <span>WATCHLIST</span>
-      <span>MOVIES</span>
-      <span>SERIES</span>
+      <ul>
+        <li><FontAwesomeIcon icon={ faHouseUser } size="lg" className="nav-icon" />HOME</li>
+        <li><FontAwesomeIcon icon={ faSearch } size="lg" className="nav-icon" /> SEARCH</li>
+        <li><FontAwesomeIcon icon={ faPlus } size="lg" className="nav-icon" /> WATCHLIST</li>
+        <li><FontAwesomeIcon icon={ faFilm } size="lg" className="nav-icon" /> MOVIES</li>
+        <li><FontAwesomeIcon icon={ faTv } size="lg" className="nav-icon" /> SERIES</li>
+      </ul>
     </div>
   )
 }
