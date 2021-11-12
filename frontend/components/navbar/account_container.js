2b0174aea3093e1ed9402b439e7e98a326973ca7
Woodstock <victorhe97@gmail.com>
2021-11-11 23:37:07 -0500

    Account component now renders again after changing the currentProfile mSTP in the AccountContainer.

--- frontend/components/navbar/account_container.js
+++ frontend/components/navbar/account_container.js
@@ -3,11 +3,11 @@ import { connect } from 'react-redux';
 import Account from './account';
 
 const mSTP = (state) => ({
-  currentProfile: state.entities.profiles[state.session.currentProfile],
+  currentProfile: state.session.currentProfile,
 });
 
 const mDTP = (dispatch) => ({
-  logout: () => dispatch(logout())
+  logout: () => dispatch(logout()),
 });
 
 export default connect(mSTP, mDTP)(Account);
