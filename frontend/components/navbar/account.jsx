8e09bb092bdcb31cf09d92e4a1d55018107bcd05
Woodstock <victorhe97@gmail.com>
2021-11-12 00:08:01 -0500

    Used hooks to rewrite Account component.

--- frontend/components/navbar/account.jsx
+++ frontend/components/navbar/account.jsx
@@ -1,62 +1,43 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+function Account({ currentProfile, logout }) {
+  const [show, setShow] = useState(false)
 
-export default class Account extends Component {
-  constructor(props) {
-    super(props)
+  const handleMouseEnterOnAccountMenu = () => setShow(show => !show)
+  const handleMouseLeaveOnAccountMenu = () => setShow(show => !show)
 
-    this.state = {
-      showAccountMenu: false
-    }
-    this.handleMouseEnterOnAccountMenu = this.handleMouseEnterOnAccountMenu.bind(this)
-    this.handleMouseLeaveOnAccountMenu = this.handleMouseLeaveOnAccountMenu.bind(this)
-  }
+  if (!currentProfile) return null;
 
-  handleMouseEnterOnAccountMenu() {
-    this.setState({
-      showAccountMenu: true
-    })
-  }
-
-  handleMouseLeaveOnAccountMenu() {
-    this.setState({
-      showAccountMenu: false
-    })
-  }
-
-  render() {
-    if (!this.props.currentProfile) return null;
-    const { logout, currentProfile } = this.props
-
-    return (
-      <div
-        onMouseEnter={() => this.handleMouseEnterOnAccountMenu()}
-        onMouseLeave={() => this.handleMouseLeaveOnAccountMenu()}
-        className="account-info"
-      >
-        <div className="greeting-container">
-          <h1 className="greeting">Greetings { currentProfile.name }</h1>
-          <img
-            src={currentProfile.avatar}
-            className="active-profile-avatar"
-            alt="current user's avatar"
-          />
-        </div>
-        {
-          this.state.showAccountMenu ? (
-            <ul className="account-menu">
-              <li><Link to="/profiles">Select Profile</Link></li>
-              <li><a href="https://github.com/VictorHeDev/DestinyPlus">GITHUB</a></li>
-              <li><a href="https://www.linkedin.com/in/victor-he/">LINKEDIN</a></li>
-              <li><a href="https://angel.co/u/victor-he">ANGELLIST</a></li>
-              <li onClick={() => logout()}>LOG OUT</li>
-            </ul>
-          ) : (
-            null
-          )
-        }
+  return (
+    <div
+      onMouseEnter={() => handleMouseEnterOnAccountMenu()}
+      onMouseLeave={() => handleMouseLeaveOnAccountMenu()}
+      className="account-info"
+    >
+      <div className="greeting-container">
+        <h1 className="greeting">Greetings { currentProfile.name }</h1>
+        <img
+          src={currentProfile.avatar}
+          className="active-profile-avatar"
+          alt="current user's avatar"
+        />
       </div>
-    )
-  }
+      {
+        show ? (
+          <ul className="account-menu">
+            <li><Link to="/profiles">Select Profile</Link></li>
+            <li><a href="https://github.com/VictorHeDev/DestinyPlus">GITHUB</a></li>
+            <li><a href="https://www.linkedin.com/in/victor-he/">LINKEDIN</a></li>
+            <li><a href="https://angel.co/u/victor-he">ANGELLIST</a></li>
+            <li onClick={() => logout()}>LOG OUT</li>
+          </ul>
+        ) : (
+          null
+        )
+      }
+    </div>
+  )
 }
+
+export default Account
