fb64f3650d217cf94cc4196f165afc7796fa6259
Woodstock <victorhe97@gmail.com>
2021-12-07 12:50:35 -0500

    Added main carousel pictures to images folder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  # config.active_job.queue_adapter = :inline
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
