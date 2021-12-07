f2b8f91f8347ab162ba6c022cd82e2f57c746be1
Woodstock <victorhe97@gmail.com>
2021-12-07 12:55:53 -0500

    npm installed slick carousel and added imported to top of the carousel jsx file

--- frontend/components/browse/carousel.jsx
+++ frontend/components/browse/carousel.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Slider from 'react-slick';
 
 export default function Carousel() {
   return (
-    <div className="main-carousel">
-      <img src={window.shangchiCarouselURL} alt="" />
+    <div className='main-carousel'>
+      <img src={window.shangchiCarouselURL} alt='' />
     </div>
-  )
+  );
 }
