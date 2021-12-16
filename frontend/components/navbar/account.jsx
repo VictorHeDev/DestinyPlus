05155932cb61245f54c8a578d4754c2b109c7e5d
Woodstock <victorhe97@gmail.com>
2021-12-16 12:02:45 -0500

    Added open links in another tab for Account component

--- frontend/components/navbar/account.jsx
+++ frontend/components/navbar/account.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function Account({ currentProfile, logout }) {
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
-  const handleMouseEnterOnAccountMenu = () => setShow(show => !show)
-  const handleMouseLeaveOnAccountMenu = () => setShow(show => !show)
+  const handleMouseEnterOnAccountMenu = () => setShow((show) => !show);
+  const handleMouseLeaveOnAccountMenu = () => setShow((show) => !show);
 
   if (!currentProfile) return null;
 
@@ -13,31 +13,53 @@ function Account({ currentProfile, logout }) {
     <div
       onMouseEnter={() => handleMouseEnterOnAccountMenu()}
       onMouseLeave={() => handleMouseLeaveOnAccountMenu()}
-      className="account-info"
+      className='account-info'
     >
-      <div className="greeting-container">
-        <h1 className="greeting">Greetings { currentProfile.name }</h1>
+      <div className='greeting-container'>
+        <h1 className='greeting'>Greetings {currentProfile.name}</h1>
         <img
           src={currentProfile.avatar}
-          className="active-profile-avatar"
+          className='active-profile-avatar'
           alt="current user's avatar"
         />
       </div>
-      {
-        show ? (
-          <ul className="account-menu">
-            <li><Link to="/profiles">Select Profile</Link></li>
-            <li><a href="https://github.com/VictorHeDev/DestinyPlus">GITHUB</a></li>
-            <li><a href="https://www.linkedin.com/in/victor-he/">LINKEDIN</a></li>
-            <li><a href="https://angel.co/u/victor-he">ANGELLIST</a></li>
-            <li onClick={() => logout()}>LOG OUT</li>
-          </ul>
-        ) : (
-          null
-        )
-      }
+      {show ? (
+        <ul className='account-menu'>
+          <li>
+            <Link to='/profiles'>Select Profile</Link>
+          </li>
+          <li>
+            <a
+              href='https://github.com/VictorHeDev/DestinyPlus'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              GITHUB
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://www.linkedin.com/in/victor-he/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              LINKEDIN
+            </a>
+          </li>
+          <li>
+            <a
+              href='https://angel.co/u/victor-he'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              ANGELLIST
+            </a>
+          </li>
+          <li onClick={() => logout()}>LOG OUT</li>
+        </ul>
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default Account
+export default Account;
